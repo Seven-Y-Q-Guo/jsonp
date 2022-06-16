ce4f3f360fb8f27ca9757f4809b5317a3e522151
Seven <seven@192.168.2.179>
2022-06-16 23:17:58 +0800

    error callback

--- jsonp.js
+++ jsonp.js
@@ -1,13 +1,13 @@
-function jsonp(url, name, cb) {
-  window[name] = cb;
+function jsonp(url, name, success, error) {
+  window[name] = success;
   const script = document.createElement('script');
   const u = new URL(url);
   const params = new URLSearchParams(u.search);
   params.set('callback', name);
   script.src = u.origin + u.pathname + '?' + params.toString();
 
-  script.onerror = function() {
-    console.log('error');
+  script.onerror = () => {
+    error('Can\'t get url');
   }
 
   document.body.append(script);
