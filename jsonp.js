b719a735e1f872d9355f40461282f9e963f8bd35
Seven <seven@192.168.2.179>
2022-06-16 11:13:01 +0800

    step10: import jsonp as lib

--- jsonp.js
+++ jsonp.js
@@ -1,3 +1,10 @@
-hello({
-  name: 'seven'
-});
+function jsonp(url, name, cb) {
+  window[name] = cb;
+  const script = document.createElement('script');
+  const u = new URL(url);
+  const params = new URLSearchParams(u.search);
+  params.set('callback', name);
+  script.src = u.origin + u.pathname + '?' + params.toString();
+
+  document.body.append(script);
+}
