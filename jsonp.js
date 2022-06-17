eec6138da198e97f77795db33f67aebf82ade281
Seven <seven@192.168.2.179>
2022-06-17 09:10:59 +0800

    handle cleanup

--- jsonp.js
+++ jsonp.js
@@ -1,19 +1,31 @@
 function jsonp(url, { name, success, error, timeout = 3000 }) {
-  window[name] = success;
   const script = document.createElement('script');
   const u = new URL(url);
   const params = new URLSearchParams(u.search);
   params.set('callback', name);
   script.src = u.origin + u.pathname + '?' + params.toString();
 
-  script.onerror = () => {
-    error('Can\'t get url');
-  }
-
-  setTimeout(function () {
-    window[name] = () => {};
+  const timer = setTimeout(function () {
+    cleanup();
     error('timeout');
   }, timeout);
 
+  function cleanup() {
+    window[name] = () => {};
+    clearTimeout(timer);
+  }
+
+  window[name] = (data) => {
+    cleanup();
+    success(data);
+  };
+
+  script.onerror = () => {
+    cleanup();
+    error('Can\'t get url');
+  }
+
   document.body.append(script);
+
+  return cleanup
 }
