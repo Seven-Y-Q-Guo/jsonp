45f27c1927e3361992c33ece85a04e00c9422530
Seven <seven@192.168.2.179>
2022-06-17 08:42:36 +0800

    spread parameters

--- jsonp.js
+++ jsonp.js
@@ -1,13 +1,13 @@
-function jsonp(url, opts) {
-  window[opts.name] = opts.success;
+function jsonp(url, { name, success, error }) {
+  window[name] = success;
   const script = document.createElement('script');
   const u = new URL(url);
   const params = new URLSearchParams(u.search);
-  params.set('callback', opts.name);
+  params.set('callback', name);
   script.src = u.origin + u.pathname + '?' + params.toString();
 
   script.onerror = () => {
-    opts.error('Can\'t get url');
+    error('Can\'t get url');
   }
 
   document.body.append(script);
