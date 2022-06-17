16600387ac0c44b07b619dac5412f0a60e8b1552
Seven <seven@192.168.2.179>
2022-06-17 16:10:24 +0800

    timeout test

--- test/jsonp.test.js
+++ test/jsonp.test.js
@@ -30,11 +30,31 @@ test('404', () => {
   jsonp(`http://xxx?${queryString}`, {
     name: 'hello',
     success: (info) => {
-      expect(info).toEqual(obj);
+      // No success
     },
     error: (error) => {
       console.log(error);
       expect(error).toBe('Can\'t get url');
+    }
+  });
+});
+
+test('timeout', () => {
+  const obj = {
+    name: 'seven',
+    gender: 'male',
+    delay: 7
+  };
+  const searchParams = new URLSearchParams(obj);
+  const queryString = searchParams.toString();
+
+  jsonp(`http://xxx?${queryString}`, {
+    name: 'hello',
+    success: (info) => {
+      // No success
+    },
+    error: (error) => {
+      expect(error).toBe('timeout');
     }
   });
 });
